030cd1b53cf05ac543ca2ba4e3c8d6795f6b5c65
jcannon8 <142158360+jcannon8@users.noreply.github.com>
2026-03-13 13:50:37 -0500

    Update tcr33minR.sh

--- Run/tcr33minR.sh
+++ Run/tcr33minR.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b
-size 1
+oid sha256:ae771351e4fdb0fc3df4750cd52644df53e1bdb999993eccfa817fbc6f52edb2
+size 2612
