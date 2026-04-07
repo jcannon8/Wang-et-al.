e3d5ea442185eeb4c93d356bad8e767cf3adae1e
John F. Cannon <142158360+jcannon8@users.noreply.github.com>
2026-04-07 07:34:56 -0500

    Update anTcr38k.tcl

--- Figures/anTcr38k.tcl
+++ Figures/anTcr38k.tcl
@@ -1,4 +1,4 @@
-# anTcr38k.tcl: PLot tcr38b & tcr39b lipid distribution (Fig 18).
+# anTcr38k.tcl: PLot tcr38.*.** & tcr39.*.** lipid distribution (Fig 18).
 # Single 405 ns (eq85) frame analyzed.
 # vmd -dispdev none
 cd /t/tcr38an
